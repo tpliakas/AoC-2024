2705b1115c8583bc31179af344f2cffe53018ccd
Ted Pliakas <ted.pliakas@booking.com>
2024-12-02 13:23:25 +0100

    day 1

---
+++ day1/2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"aoc/utils"
+)
+
+func main() {
+	contents, err := utils.ReadFileContents("day1/input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var leftList, rightList []int
+	lines := strings.Split(contents, "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		numbers := strings.Fields(line)
+		if len(numbers) != 2 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		num1, err1 := strconv.Atoi(numbers[0])
+		num2, err2 := strconv.Atoi(numbers[1])
+		if err1 != nil || err2 != nil {
+			fmt.Println("Error parsing numbers on line:", line)
+			continue
+		}
+
+		leftList = append(leftList, num1)
+		rightList = append(rightList, num2)
+	}
+
+	rightCount := make(map[int]int)
+	for _, num := range rightList {
+		rightCount[num]++
+	}
+
+	var similarityScore int
+	for _, num := range leftList {
+		count, exists := rightCount[num]
+		if exists {
+			similarityScore += num * count
+		}
+	}
+
+	fmt.Println("Total similarity score:", similarityScore)
+}
